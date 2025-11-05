b3c70b1bc8f02d51ab68eecc1d7b55fbc211e4a7
AlejandroS <3720366@alu.murciaeduca.es>
2025-11-05 09:37:57 +0100

    Rutas familias-profesionales

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+
+Route::get('/', function()
+{
+    return 'Pantalla principal';
 });
+
+Route::get('login', function()
+{
+    return 'Login usuario';
+});
+
+Route::get('logout', function()
+{
+    return 'Logout usuario';
+});
+
+Route::prefix('familias-profesionales')->group(function () {
+    Route::get('/', function () {
+        return 'Listado familias profesionales';
+    });
+    Route::get('/show/{id}', function ($id) {
+        return 'Vista detalle familia profeisonal ' . $id;
+    })->where('id', '[0-9]+');
+    Route::get('/create', function () {
+        return 'Añadir familia profesional';
+    });
+        Route::get('/edit/{id}', function ($id) {
+        return 'Modificar familia profesional ' . $id;
+    })->where('id', '[0-9]+');
+
+});
+
+Route::get('perfil/{id?}', function($id = null)
+{
+    if ($id) {
+        return 'Visualizar el usuario de ' . $id;
+    }else {
+        return 'Visualizar el usuario propio';
+    }
+})->where('id', '[0-9]+');

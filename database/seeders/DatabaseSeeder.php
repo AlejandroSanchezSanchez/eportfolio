f1bed7211739be1f3a7cbb534ec3da978f8438d2
AlejandroS <3720366@alu.murciaeduca.es>
2025-12-04 10:03:13 +0100

    Familias Profesionales Tablas BD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Model::unguard();
+        Schema::disableForeignKeyConstraints();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // llamadas a otros ficheros de seed
+        $this->call(FamiliasProfesionalesTableSeeder::class);
+
+        // llamadas a otros ficheros de seed
+
+        Model::reguard();
+
+        Schema::enableForeignKeyConstraints();
     }
 }

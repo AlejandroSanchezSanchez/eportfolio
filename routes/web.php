4a2ee5590075a1652b6ccb7c69cfbe1aac93e302
AlejandroS <3720366@alu.murciaeduca.es>
2026-02-17 09:49:44 +0100

    Consumiendo API públicas

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\FamiliasProfesionalesController;
 use App\Http\Controllers\ResultadosAprendizajeController;
 use App\Http\Controllers\MatriculasController;
+use App\Http\Controllers\PortfolioImportController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -23,6 +24,24 @@ Route::get('login', function () {
 });
 Route::get('logout', function () {
     return "Logout usuario";
+});
+
+
+
+
+
+Route::middleware(['auth'])->group(function () {
+    // Formulario de importación
+    Route::get('/portfolio/import', [PortfolioImportController::class, 'showImportForm'])
+        ->name('portfolio.import.index');
+
+    // Importar desde JSON Resume
+    Route::post('/portfolio/import/json-resume', [PortfolioImportController::class, 'importJsonResume'])
+        ->name('portfolio.import.json-resume');
+
+    // Importar desde GitHub
+    Route::post('/portfolio/import/github', [PortfolioImportController::class, 'importGitHub'])
+        ->name('portfolio.import.github');
 });
 
 
